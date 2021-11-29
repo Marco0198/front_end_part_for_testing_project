db659a9f40b48bc18f7f3f95ca0b24cea5e2126f
Marco0198 <219049505@mycput.ac.za>
2021-11-29 16:32:21 +0200

    i deleted some content

--- src/views/pages/About.vue
+++ src/views/pages/About.vue
@@ -2,25 +2,7 @@
  <Layout name="LayoutDefault">
    
     <div> <h1>about page</h1>
-    <b-card  style="width: auto">
-    <form>
-  <div class="form-row">
-    <div class="form-group col-md-6"> 
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-    </div>
-    <div class="form-group col-md-6">
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
-    <div class="form-group col-md-6">
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-    </div>
-    <div class="form-group col-md-6">
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
-</b-card>
+ 
 </div>
 
 
